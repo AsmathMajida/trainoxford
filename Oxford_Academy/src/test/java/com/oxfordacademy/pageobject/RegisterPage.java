1f8ab4ec03651fbbd533fe5836c382b9c6283850
AsmathMajida <60865968+AsmathMajida@users.noreply.github.com>
2020-03-25 14:56:57 +0530

    Update RegisterPage.java

--- Oxford_Academy/src/test/java/com/oxfordacademy/pageobject/RegisterPage.java
+++ Oxford_Academy/src/test/java/com/oxfordacademy/pageobject/RegisterPage.java
@@ -18,27 +18,32 @@ public class RegisterPage
 	By password=By.name("Password");
 	By confirm_password=By.id("ConfirmPassword");
 	By submit=By.xpath("//*[@id=\"registerBtn\"]");
-
-	public void launchBrowser(String browser) //Launch url
+        //Launching url
+	public void launchBrowser(String browser) 
 	{
 		try
 		{
+			//Launching firefox page
 			if(browser.equalsIgnoreCase("firefox"))
 			{
 				System.setProperty("webdriver.gecko.driver","src\\test\\resources\\Driver\\geckodriver.exe");
 				driver = new FirefoxDriver();
 			}
+			//Launching chrome page
 			else if(browser.equalsIgnoreCase("chrome"))
 			{
 				System.setProperty("webdriver.chrome.driver","src/test/resources/Driver/chromedriver.exe");
 				driver = new ChromeDriver();
 			}
+			//Launching Internet Explorer page
 			else if(browser.equalsIgnoreCase("Internet Explore"))
 			{
 				System.setProperty("webdriver.ie.driver","src\\test\\resources\\Driver\\IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
 			}
+		//maximizing the window
 		driver.manage().window().maximize();
+		//Adding implicit wait
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		}
 		catch(WebDriverException e)
@@ -47,13 +52,14 @@ public class RegisterPage
 		}
 		
 	}
+	       //Launching the website
 		public void oxfordHomepage()
 		{
-			driver.get("https://academic.oup.com/journals");//Launching the website
+			driver.get("https://academic.oup.com/journals");
 			System.out.println(driver.getTitle());
 		}
 		//Enter the details for valid registration
-		public void registerDetails() throws InterruptedException//Entering the details in the required field
+		public void registerDetails() throws InterruptedException
 		{
 			driver.findElement(register).click();
 			driver.findElement(email).sendKeys("majida.maji98@gmail.com");
@@ -71,10 +77,11 @@ public class RegisterPage
 			driver.findElement(submit).sendKeys(Keys.ENTER);
 			Thread.sleep(5000);
 		}
+        	//closing the website
 		public void exit() throws InterruptedException
 		{
 			Thread.sleep(5000);
-			driver.close();//closing the website
+			driver.close();
 		}
 
 }
